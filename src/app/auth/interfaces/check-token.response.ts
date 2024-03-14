b4752432eeb5b1c8f8184e1a61f40e2f50754b81
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-03-13 18:05:58 -0600

    Cambios finales en la aplicación

---
+++ src/app/auth/interfaces/check-token.response.ts
@@ -0,0 +1,8 @@
+import { User } from "./user.interfaces";
+
+export interface CheckTokenResponse {
+    user:  User;
+    token: string;
+}
+
+
---
+++ src/app/auth/interfaces/index.ts
@@ -0,0 +1,4 @@
+export * from './auth-status.enum';
+export * from './check-token.response';
+export * from './login-response.interfaces';
+export * from './user.interfaces';
